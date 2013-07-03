841168710d126078971d8e90a286fc99a1f468bb
schinken <schinken@hackerspace-bamberg.de>
2013-07-03 14:13:51 +0200

    Using api key

--- mediawiki.py
+++ mediawiki.py
@@ -15,6 +15,8 @@ project = 'backspace'
 api_issues = settings.http_url+'/projects/'+project+'/issues.json?&set_filter=1&f[]=status_id&op[status_id]=*&f[]=is_private&op[is_private]=%3D&v[is_private][]=0'
 api_auth = (settings.http_user, settings.http_pass)
 
+api_header = {'X-Redmine-API-Key': settings.api_key}
+
 # We explicitly don't use the API key here to access the tickets, to avoid
 # retrieving private tickets. theres no field to indicate if its private
 # or not
@@ -40,7 +42,8 @@ issues_by_category = defaultdict(list)
 while offset < total_count:
     
     url = api_issues+'&offset='+str(offset)+'&limit='+str(offset_add)
-    result = requests.get(url, auth=api_auth, verify=False).json()
+    result = requests.get(url, auth=api_auth, verify=False, 
+                          headers=api_header).json()
  
     for issue in result['issues']:
 
