f13a4bd7bc389bc3e73787d591f44620976867cf
schinken <schinken@hackerspace-bamberg.de>
2013-07-05 18:59:32 +0200

    added summary mail

--- create_summary.py
+++ create_summary.py
@@ -1,52 +1,17 @@
 import requests
 import settings
+import redmine
+import os
 
-import dateutil.parser
-from datetime import datetime, timedelta
-
-lastrun = timedelta(days=7)
+from jinja2 import Environment, FileSystemLoader
 
 project = 'backspace'
- 
-users_existing = []
-users_ingroup = []
- 
-api_issues = settings.http_url+'/projects/'+project+'/issues.json?status_id=*'
 
-api_header = {'X-Redmine-API-Key': settings.api_key}
-api_auth = (settings.http_user, settings.http_pass)
+templates_dir = os.path.join(os.path.dirname(__file__), 'templates')
+env = Environment(loader=FileSystemLoader(templates_dir),
+                  extensions=['jinja2.ext.loopcontrols'])
 
+template = env.get_template('summary.jinja2')
+tpl_result = template.render(issues=redmine.get_issues(project))
 
-# retrieve issues
-issues = []
-datefields = ['created_on', 'updated_on', 'closed_on']
-
-offset = 0
-offset_add = 100
-total_count = 1
-
-while offset < total_count:
-    
-    url = api_issues+'&offset='+str(offset)+'&limit='+str(offset_add)
-    result = requests.get(url, auth=api_auth, headers=api_header,
-                          verify=False).json()
- 
-    for issue in result['issues']:
-
-        for field in datefields:
-            if field in issue:
-                issue[field] = dateutil.parser.parse(issue[field])
-
-        issues.append(issue)
-
-    offset = result['offset']
-    total_count = result['total_count']
-
-    if not total_count:
-        break
-
-    offset += offset_add
-
-
-print issues
-
+print tpl_result
