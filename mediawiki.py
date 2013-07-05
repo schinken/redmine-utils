91c942553f0be54304b6fbde9b48755cb63e62c2
schinken <schinken@hackerspace-bamberg.de>
2013-07-05 10:30:53 +0200

    using redmine lib to retrieve issues

--- mediawiki.py
+++ mediawiki.py
@@ -1,6 +1,6 @@
-import requests
 import settings
 import mwclient
+import redmine
 import os
 
 from jinja2 import Environment, FileSystemLoader
@@ -11,15 +11,6 @@ from dateutil import parser
 from collections import defaultdict
 
 project = 'backspace'
-
-api_issues = settings.http_url+'/projects/'+project+'/issues.json?set_filter=1&f[]=status_id&op[status_id]=*&f[]=is_private&op[is_private]=%3D&v[is_private][]=0'
-api_auth = (settings.http_user, settings.http_pass)
-
-api_header = {'X-Redmine-API-Key': settings.api_key}
-
-# We explicitly don't use the API key here to access the tickets, to avoid
-# retrieving private tickets. theres no field to indicate if its private
-# or not
 
 wiki_time = datetime.now()
 wiki_update = False
@@ -39,30 +30,16 @@ total_count = 1
 issues_by_category = defaultdict(list)
 
 # retrieve issues
-while offset < total_count:
-    
-    url = api_issues+'&offset='+str(offset)+'&limit='+str(offset_add)
-    result = requests.get(url, auth=api_auth, verify=False, 
-                          headers=api_header).json()
- 
-    for issue in result['issues']:
+for issue in redmine.get_issues(project):
 
-        if 'closed_on' in issue:
-            continue
+    if 'closed_on' in issue:
+        continue
 
-        if parser.parse(issue['updated_on']).replace(tzinfo=None) > wiki_time:
-            wiki_update = True
+    if parser.parse(issue['updated_on']).replace(tzinfo=None) > wiki_time:
+        wiki_update = True
 
-        tracker = issue['tracker']['name']
-        issues_by_category[tracker].append(issue)
-
-    offset = result['offset']
-    total_count = result['total_count']
-
-    if not total_count:
-        break
-
-    offset += offset_add
+    tracker = issue['tracker']['name']
+    issues_by_category[tracker].append(issue)
 
 
 templates_dir = os.path.join(os.path.dirname(__file__), 'templates')
